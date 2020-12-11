9d5d7c7c78185adbb47e6283fd79a3fcce6dfa2f
sergey_petrochenko <proffsneip@gmail.com>
2020-12-11 19:39:49 +0300

    Added date field to item note

--- app/src/main/java/com/droid/notebook/core/viewModels/NoteDetailsViewModel.kt
+++ app/src/main/java/com/droid/notebook/core/viewModels/NoteDetailsViewModel.kt
@@ -1,14 +1,12 @@
 package com.droid.notebook.core.viewModels
 
-import android.annotation.SuppressLint
 import androidx.hilt.Assisted
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.SavedStateHandle
 import androidx.lifecycle.ViewModel
 import com.droid.notebook.core.services.interfaces.NotesService
-import com.droid.notebook.utils.constants.Constants
+import com.droid.notebook.data.Note
 import com.droid.notebook.utils.navigator.AppNavigator
-import java.text.SimpleDateFormat
 
 class NoteDetailsViewModel @ViewModelInject constructor(
     private val notesService: NotesService,
@@ -21,13 +19,12 @@ class NoteDetailsViewModel @ViewModelInject constructor(
     var date: String = ""
     var isEditMode: Boolean = false
 
-    @SuppressLint("SimpleDateFormat")
     fun initViewModel(noteId: Int) {
         val note = notesService.single { it.id == noteId }
         if (note != null) {
             title = note.title
             description = note.description
-            date = SimpleDateFormat(Constants.dateFormat).format(note.date)
+            date = note.date
             isEditMode = true
         } else {
             isEditMode = false
@@ -35,7 +32,8 @@ class NoteDetailsViewModel @ViewModelInject constructor(
     }
 
     fun saveNoteCommand() {
-
+        val note = Note(11, this.title, this.description, this.date)
+        notesService.createNote(note)
     }
 
     fun goBackCommand() {
--- app/src/main/java/com/droid/notebook/data/Note.kt
+++ app/src/main/java/com/droid/notebook/data/Note.kt
@@ -1,5 +1,3 @@
 package com.droid.notebook.data
 
-import java.util.*
-
-data class Note(val id: Int, val title: String, val description: String, val date: Date)
+data class Note(val id: Int, val title: String, val description: String, val date: String)
